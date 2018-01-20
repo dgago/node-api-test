8f55e5ddfc8be2837bfb88d49e07056dbf9132cc
Diego Gago <diego.gago@gmail.com>
2018-01-20 01:19:52 -0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbName = "z";
-const url = `mongodb://13.65.32.65:17510/${dbName}`;
+const url = process.env.MONGODB_URI || `mongodb://13.65.32.65:17510/${dbName}`;
 
 mongoose.PromiseProvider = global.Promise;
 mongoose.connect(url);
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var { User } = require("./models/user");
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 // POST /todos
@@ -65,8 +67,8 @@ app.get("/todos/:id", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log("Escuchando en el puerto 3000.");
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}.`);
 });
 
 module.exports = { app };
