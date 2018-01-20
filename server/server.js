f71abf6139b0e462ac66de9a60650e77ef7ba2b3
Diego Gago <diego.gago@gmail.com>
2018-01-20 09:42:30 -0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var { ObjectID } = require("mongodb");
-var { mongoose } = require("./db/mongoose");
-var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
+const { ObjectID } = require("mongodb");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
-var app = express();
+const app = express();
 
 const port = process.env.PORT || 3000;
 
@@ -87,6 +88,37 @@ app.delete("/todos/:id", (req, res) => {
       return res.status(400).send(e);
     }
   );
+});
+
+// PATCH /todos/:id
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({
+      message: "El identificador ingresado no es válido."
+    });
+  }
+
+  var body = _.pick(req.body, ["text", "completed"]);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      return res.send({ todo });
+    })
+    .catch((e) => {
+      return res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
