158d718f331fddf6e129684d53636a73f8a7819b
Diego Gago <diego.gago@gmail.com>
2018-01-20 16:19:26 -0300

    Added password hashing to user

--- playground/hashing.js
+++ playground/hashing.js
@@ -34,3 +34,20 @@ console.log(token);
 
 var decoded = jwt.verify(token, secret);
 console.log(decoded);
+
+
+
+const bcrypt = require("bcryptjs");
+var password = "abc123";
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = "$2a$10$ofvSTeokKmZzYo.f1A.gzeZPHCsOM9SohZ/66WSiO0GevpY6tAxvi";
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+  console.log(err);
+});
