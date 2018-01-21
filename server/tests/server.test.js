508196d0095d24f4d9910e5b85ba587231afc6cf
Diego Gago <diego.gago@gmail.com>
2018-01-20 23:58:46 -0300

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -262,11 +262,13 @@ describe("POST /users", () => {
           return done(err);
         }
 
-        User.findOne({ email: user.email }).then((user) => {
-          expect(user);
-          // expect(user.password);
-          done();
-        });
+        User.findOne({ email: user.email })
+          .then((user) => {
+            expect(user);
+            // expect(user.password);
+            done();
+          })
+          .catch((e) => done(e));
       });
   });
 
@@ -292,5 +294,58 @@ describe("POST /users", () => {
       .send(user)
       .expect(400)
       .end(done);
+  });
+});
+
+describe("POST /users/login", () => {
+  it("should login user and return auth token", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers["x-auth"]).to.be.a("string");
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0].access).eql("auth");
+            expect(user.tokens[0].token).eql(res.headers["x-auth"]);
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it("should reject invalid login", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({
+        email: users[1].email,
+        password: "pwd"
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers["x-auth"]).eql(undefined);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens.length).eql(0);
+            done();
+          })
+          .catch((e) => done(e));
+      });
   });
 });
