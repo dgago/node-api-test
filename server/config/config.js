9f0420bc583aa2d7e94f7eccd55d79db4ba76199
Diego Gago <diego.gago@gmail.com>
2018-01-21 16:39:11 -0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 const env = process.env.NODE_ENV || "development";
 console.log("env *****", env);
 
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = `mongodb://13.65.32.65:17510/z`;
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = `mongodb://13.65.32.65:17510/z2`;
+if (env === "development" || env === "test") {
+  var config = require("./config.json");
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
